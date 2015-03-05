c7012af6d5269929e2cbaaddf664140fec79f67b
Sonia Martínez Martín <inmakia@gmail.com>
2015-03-05 12:25:16 +0100

    Add clase TemaEntity

---
+++ JEE_ECP/src/main/java/models/entities/TemaEntity.java
@@ -0,0 +1,43 @@
+package models.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class TemaEntity {
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private String name;
+	
+	private String question;
+	
+	public TemaEntity(String name, String question){
+		this.setName(name);
+		this.setQuestion(question);
+	}
+	
+	public TemaEntity() {
+		this(null, null);
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getQuestion() {
+		return question;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setQuestion (String question) {
+		this.question = question;
+	}
+}
