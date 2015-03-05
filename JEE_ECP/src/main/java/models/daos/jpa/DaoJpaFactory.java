0a31827dd93fe6b64b04974401aee0158746996c
Sonia Martínez Martín <inmakia@gmail.com>
2015-03-05 13:59:23 +0100

    Final empezada clase DaoJpaFactory

--- JEE_ECP/src/main/java/models/daos/jpa/DaoJpaFactory.java
+++ JEE_ECP/src/main/java/models/daos/jpa/DaoJpaFactory.java
@@ -1,5 +1,14 @@
 package models.daos.jpa;
 
-public class DaoJpaFactory {
+import javax.persistence.EntityManagerFactory;
 
+public class DaoJpaFactory {
+	private static final String PERSISTENCE_UNIT = "jee_ecp";
+	
+	private static EntityManagerFactory entityManagerFactory;
+	
+	public DaoJpaFactory() {
+	}
+	
+	
 }
