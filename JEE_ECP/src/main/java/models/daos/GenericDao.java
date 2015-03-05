b8598f3228ed37a833c115e58587d8d26eae7996
Sonia Martínez Martín <inmakia@gmail.com>
2015-03-05 10:51:28 +0100

    Add clase GenericDao

--- JEE_ECP/src/main/java/models/daos/GenericDao.java
+++ JEE_ECP/src/main/java/models/daos/GenericDao.java
@@ -1,5 +1,11 @@
 package models.daos;
 
-public class GenericDao {
+import java.util.List;
 
+public interface GenericDao<T, ID> {
+	public void create (T entity);
+	public T read (ID id);
+	public void update(T entity);
+	public void deleteById(ID id);
+	public List<T> findAll();
 }
