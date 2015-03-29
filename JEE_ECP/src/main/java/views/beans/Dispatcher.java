67a0c2b00cb3c09ba24107fc384cd24dff352bbd
Sonia Martínez Martín <inmakia@gmail.com>
2015-03-29 23:18:14 +0200

    Update quitar imports no usados

--- JEE_ECP/src/main/java/views/beans/Dispatcher.java
+++ JEE_ECP/src/main/java/views/beans/Dispatcher.java
@@ -8,9 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.daos.DaoFactory;
-import models.daos.jpa.DaoJpaFactory;
-import models.entities.TemaEntity;
 import models.utils.Studies;
 
 @WebServlet("/jsp/*")
